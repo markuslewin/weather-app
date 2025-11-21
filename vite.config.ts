6eeec52f98388e18a01ec9ffd8a44b8af54f6e0e
Markus Lewin <lewinmarkus@gmail.com>
2025-11-21 20:52:22 +0100

    Fix devtools warning

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import netlifyPlugin from "@netlify/vite-plugin-react-router";
 import { iconsSpritesheet } from "vite-plugin-icons-spritesheet";
+import devToolsJson from "vite-plugin-devtools-json";
 
 export default defineConfig({
   plugins: [
+    devToolsJson(),
     iconsSpritesheet({
       inputDir: "app/icons/svgs",
       outputDir: "app/icons",
